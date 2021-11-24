d157db7d68994357ccac220c8af0c84ca4efabdd
Matthew McRaven <matthew.mcraven@gmail.com>
2021-11-24 07:40:31 -0800

    feat(ui): add generate-react-cli template
    Also add a StoriedComponent, generated with cli tool

---
+++ packages/ui/ui-sample/src/components/StoriedComponent/StoriedComponent.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './StoriedComponent.scss';
+
+const StoriedComponent = () => <div className={'StoriedComponent'} data-testid="StoriedComponent"></div>;
+
+export default StoriedComponent;
---
+++ packages/ui/ui-sample/templates/component/TemplateName.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './TemplateName.scss';
+
+const TemplateName = () => <div className={'TemplateName'} data-testid="TemplateName"></div>;
+
+export default TemplateName;
