d42d8aaa6b08e655abf4a7787707ddbe68e32d05
Matthew McRaven <matthew.mcraven@gmail.com>
2021-11-24 10:43:16 -0800

    Fix: Attempt to force `lerna publish`

--- packages/ui-sample/src/components/Button/Button.tsx
+++ packages/ui-sample/src/components/Button/Button.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 const Button = () => {
   console.log("Hello Button");
-  return <button>Hello Button </ button>;
+  return <button>Hello Button</ button>;
 }
 export default Button;
