fe37eb3d9e81cd7c667a74610199e565521af97c
Matthew McRaven <matthew.mcraven@gmail.com>
2021-11-24 07:56:43 -0800

    feat(sample): Add storybook to packages dir
    This will collect all stories into a single book, making CI/CD very easy.

--- packages/ui-sample/src/components/StoriedComponent/StoriedComponent.tsx
+++ packages/ui-sample/src/components/StoriedComponent/StoriedComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './StoriedComponent.scss';
 
-const StoriedComponent = () => <div className={'StoriedComponent'} data-testid="StoriedComponent"></div>;
+const StoriedComponent = () => <div className={'StoriedComponent'} data-testid="StoriedComponent">Hello World</div>;
 
 export default StoriedComponent;
