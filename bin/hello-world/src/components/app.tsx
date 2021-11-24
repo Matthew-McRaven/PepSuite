b064f2b3ac644bc87639eda9e49d348715cdf4ec
Matthew McRaven <matthew.mcraven@gmail.com>
2021-11-24 07:08:27 -0800

    feat(sample): Add sample application that consume library
    Copied from: https://github.com/NiGhTTraX/ts-monorepo/tree/npm

---
+++ bin/hello-world/src/components/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Button from "@pep10/ui-sample"
+
+export const App = () => {
+  return (
+    <div>
+      <Button />
+      <h1> Hello World</h1>
+    </div>
+  );
+};
