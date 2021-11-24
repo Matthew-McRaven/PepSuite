e27b76930ffe4da98ad717ed253bebe553db8b3d
Matthew McRaven <matthew.mcraven@gmail.com>
2021-11-24 07:41:08 -0800

    New: Consume templated component in hello-world

--- bin/hello-world/src/components/app.tsx
+++ bin/hello-world/src/components/app.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-import { Button } from "@pep10/ui-sample"
+import { Button, StoriedComponent } from "@pep10/ui-sample"
 
 export const App = () => {
   return (
     <div>
       <Button />
+      <StoriedComponent />
       <h1> Hello World</h1>
-    </div>
+    </div >
   );
 };
