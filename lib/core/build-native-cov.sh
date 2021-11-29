bda1979c7577ce47862e35880a1537360c792a2b
Matthew McRaven <matthew.mcraven@gmail.com>
2021-11-29 13:52:16 -0800

    Build: Disable nested tests
    Only build tests for the current project.
    This greatly speeds up term compile process, as we can skip ISA validation tests
    
    This is a valid methodology because all tests are still run at some point; tests are no longer run multiple times.

--- lib/core/build-native-cov.sh
+++ lib/core/build-native-cov.sh
@@ -6,6 +6,6 @@ cd build
 	source /emsdk/emsdk_env.sh
 	mkdir -p coverage 
 	cd coverage
-	cmake ../.. -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DCMAKE_BUILD_TYPE=Debug -Dcode_coverage=1
+	cmake ../.. -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++) -DCMAKE_BUILD_TYPE=Debug -Dcode_coverage=1 -DENABLE_TESTING=1
 	make -j$(nproc) && make test -j$(nproc)
 )
