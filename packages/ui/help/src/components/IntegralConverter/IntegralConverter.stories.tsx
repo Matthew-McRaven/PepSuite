5d1a7428d5734d4a98cfeee143aa2ec6132f37be
Matthew McRaven <mkm302@georgetown.edu>
2022-01-09 01:57:23 +0000

    test(ui): add storybooks for integral converters

--- packages/ui/help/src/components/IntegralConverter/IntegralConverter.stories.tsx
+++ packages/ui/help/src/components/IntegralConverter/IntegralConverter.stories.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IntegralConverter from './IntegralConverter';
 
 export default {
-  title: 'IntegralConverter',
+  title: 'Help/IntegralConverter',
   component: IntegralConverter,
-  argTypes: {
-  },
+
 };
 
-const Template = () => <IntegralConverter />;
+const Template = (args: { base: 2 | 10 | 16 }) => {
+  const { base } = args;
+  const [state, setState] = useState(0);
+  return <IntegralConverter state={state} setState={setState} base={base} />;
+};
 
-export const withTemplate = Template.bind({});
+export const Decimal = Template.bind({});
+Decimal.args = { base: 10 };
+
+export const Binary = Template.bind({});
+Binary.args = { base: 2 };
+
+export const Hex = Template.bind({});
+Hex.args = { base: 16 };
