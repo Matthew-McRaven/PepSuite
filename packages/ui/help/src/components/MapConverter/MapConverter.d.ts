fee6641b3205c1d55a7651d037a6c0712b78a4ed
Matthew McRaven <mkm302@georgetown.edu>
2022-01-15 19:55:25 +0000

    fix(ui): fix mispelled type

--- packages/ui/help/src/components/MapConverter/MapConverter.d.ts
+++ packages/ui/help/src/components/MapConverter/MapConverter.d.ts
@@ -1,7 +1,7 @@
-import type { BaseConveterProps } from '../BaseConverter';
+import type { BaseConverterProps } from '../BaseConverter';
 
 // eslint-disable-next-line no-unused-vars
 export type MappingFunction = (key: number) => string
-export interface MapConverterProps extends BaseConveterProps {
+export interface MapConverterProps extends BaseConverterProps {
   map: MappingFunction
 }
