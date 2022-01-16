723118ae037db398783914094898fc52bc000e10
Matthew McRaven <mkm302@georgetown.edu>
2022-01-16 00:50:59 +0000

    build(ui): link CPU against help
    This allows me to share byte converters with CPU.

--- packages/ui/help/src/index.ts
+++ packages/ui/help/src/index.ts
@@ -2,7 +2,9 @@ import { AsciiMapConverter } from './components/AsciiMapConverter';
 import Button from './components/Button';
 import { IntegralConverter } from './components/IntegralConverter';
 import { MapConverter } from './components/MapConverter';
+import type { HigherOrderConverterProps } from './components/BaseConverter';
 
 export {
   AsciiMapConverter, Button, IntegralConverter, MapConverter,
 };
+export type { HigherOrderConverterProps };
