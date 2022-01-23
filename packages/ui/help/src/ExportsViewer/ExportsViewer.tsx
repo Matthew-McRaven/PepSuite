5d87d86caf06be28cef6e7430ead5f951dd1dc25
Matthew McRaven <matthew.mcraven@gmail.com>
2022-01-23 01:35:53 +0000

    feat(ui): render ExportViewer value with IntegralConverter
    In the future, I should allow varying bases.
    
    Progress to #333.

--- packages/ui/help/src/ExportsViewer/ExportsViewer.tsx
+++ packages/ui/help/src/ExportsViewer/ExportsViewer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ExportsViewer.scss';
+import { Integral } from '@pep10/ui-converters';
 
 export interface ExportDefinition {
   name: string
@@ -17,7 +18,16 @@ const ExportRow = (props: ExportDefinition) => {
   return (
     <div className="Row">
       <div>{name}</div>
-      <div>{value}</div>
+      <div>
+        {Integral.IntegralConverter({
+          state: value,
+          base: 16,
+          setState: () => { },
+          error: () => { },
+          isReadOnly: true,
+          byteLength: 2,
+        })}
+      </div>
       <div>{sourceDefinition}</div>
     </div>
   );
