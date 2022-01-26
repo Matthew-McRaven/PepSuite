a634f5ce33d40f61468339b43e8e9ee11ff34e3f
Matthew McRaven <matthew.mcraven@gmail.com>
2022-01-26 07:44:43 -0800

    docs(term): add support information to --about
    Docs only change, so [skip ci]
    
    Progress towards #345.

--- bin/js-term/src/about.ts
+++ bin/js-term/src/about.ts
@@ -2,13 +2,17 @@
 const aboutText = 'Check for updates:\n\
 https://gitlab.com/pep10/pepsuite/-/releases\n\
 \n\
+Support\n\
+    Visit https://gitlab.com/pep10/pepsuite/-/issues/ to view existing issues or raise new ones.\n\
+    Alternatively, contact Matthew McRaven (matthew.mcraven@gmail.com) for support.\n\
+\n\
 Authors\n\
-    Matthew McRaven(matthew.mcraven@gmail.com)\n\
-    J.Stanley Warford(Stan.Warford@pepperdine.edu)\n\
-    Emily Dimpfl(Emily.Dimpfl@gmail.com)\n\
+    Matthew McRaven (matthew.mcraven@gmail.com) \n\
+    J. Stanley Warford (Stan.Warford@pepperdine.edu) \n\
+    Emily Dimpfl (Emily.Dimpfl@gmail.com) \n\
 \n\
 Licensing\n\
-    PepTerm is distributed under an Open Source license. See the\n\
+    PepTerm is distributed under an Open Source license.See the\n\
     file "LICENSE" in the github source distribution for information on\n\
     terms & conditions for accessing and otherwise using Pep9Term and for\n\
     a DISCLAIMER OF ALL WARRANTIES.';
