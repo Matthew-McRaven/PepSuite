33ec7252e6c9895c924d7e9c3024022082b4cb0a
Matthew McRaven <mkm302@georgetown.edu>
2022-01-31 06:32:56 +0000

    build(core): stop including nonexistant Pep9 module

--- packages/native/core/index.js
+++ packages/native/core/index.js
@@ -3,17 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.pep9 = exports.pep10 = void 0;
+exports.pep10 = void 0;
 
 var _pep = _interopRequireDefault(require("./dist/pep10.js"));
-
-var _pep2 = _interopRequireDefault(require("./dist/pep9.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Import the default export (the module) and rename.
 // Renaming is necessary because each emscripten module has its own `Module`.
-const pep10 = (0, _pep.default)();
+//import { default as _pep9 } from "./dist/pep9.js"
+const pep10 = (0, _pep.default)(); //const pep9 = _pep9()
+
 exports.pep10 = pep10;
-const pep9 = (0, _pep2.default)();
-exports.pep9 = pep9;
