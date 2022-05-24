f14904b0b904575dd8dddd59460dd6928d41d5f2
Matthew McRaven <mkm302@georgetown.edu>
2022-05-24 15:59:34 +0000

    feat(core): implment peph,pepb output
    Now just need a terminal frontend.
    Progress towards #382, #378, #212.

--- packages/native/core/src/libisa/utils/format.cpp
+++ packages/native/core/src/libisa/utils/format.cpp
@@ -1,15 +1,64 @@
 
 #include "format.hpp"
 
+#include <iostream>
+#include <sstream>
+
 #include <fmt/core.h>
 
-std::string utils::bytes_to_hex_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, bool include_zz) {
-    std::string hex_string = "";
-    for (int i = 0; i < bytes.size(); i++) {
-        hex_string += (fmt::format("{:02x}", bytes[i]));
-        hex_string += (((i % bytes_per_line) == bytes_per_line - 1) ? "\n" : " ");
+std::string utils::bytes_to_nbit_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, uint8_t base,
+                                        uint8_t interchar_space, bool include_zz, bool include_spaces) {
+    // Function called very often, so pre-allocate format strings
+    const std::string format_2 = "{:08b}";
+    const std::string format_8 = "{:04o}";
+    const std::string format_16 = "{:02X}";
+
+    // Write to stringstream instead of appending to string, supposed to be faster, need benchmark.
+    std::stringstream ss;
+
+    // Choose the appropriate format string based on the base.
+    const std::string *fmt = nullptr;
+    switch (base) {
+    case 2:
+        fmt = &format_2;
+        break;
+    case 8:
+        fmt = &format_8;
+        break;
+    case 16:
+        fmt = &format_16;
+        break;
     }
+
+    // Check for valid base, otherwise vformat would explode.
+    if (fmt == nullptr)
+        throw std::invalid_argument("base must be in {2,8,16}");
+    else if (interchar_space <= 0)
+        throw std::invalid_argument("interchar_space must be positive");
+    // Expand bytes into strings of a given base, optionally space/newline delimited.
+    for (int i = 0; i < bytes.size(); i++) {
+        auto formatted = fmt::vformat(*fmt, fmt::make_format_args(bytes[i]));
+        // Insert a space every `interchar_space` characters, except don't add one at the end.
+        for (auto split = 0; split < formatted.size(); split += interchar_space) {
+            ss << formatted.substr(split, interchar_space);
+            if (split + interchar_space < formatted.size())
+                ss << " ";
+        }
+        if (include_spaces)
+            ss << (((i % bytes_per_line) == bytes_per_line - 1) ? "\n" : " ");
+    }
+
     if (include_zz)
-        hex_string.append("zz");
-    return hex_string;
+        ss << "zz";
+    return ss.str();
+}
+
+std::string utils::bytes_to_hex_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, bool include_zz,
+                                       bool include_spaces, uint8_t interchar_space) {
+    return bytes_to_nbit_string(bytes, bytes_per_line, 16, interchar_space, include_zz, include_spaces);
+}
+
+std::string utils::bytes_to_bit_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, bool include_zz,
+                                       bool include_spaces, uint8_t interchar_space) {
+    return bytes_to_nbit_string(bytes, bytes_per_line, 2, interchar_space, include_zz, include_spaces);
 }
--- packages/native/core/src/libisa/utils/format.hpp
+++ packages/native/core/src/libisa/utils/format.hpp
@@ -5,5 +5,11 @@
 #include <vector>
 
 namespace utils {
-std::string bytes_to_hex_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, bool include_zz = true);
-}
+// Throws for bases other than 2,8,16.
+std::string bytes_to_nbit_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, uint8_t base,
+                                 uint8_t interchar_space, bool include_zz = true, bool include_spaces = true);
+std::string bytes_to_hex_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, bool include_zz = true,
+                                bool include_spaces = true, uint8_t interchar_space = 2);
+std::string bytes_to_bit_string(const std::vector<uint8_t> &bytes, uint16_t bytes_per_line, bool include_zz = true,
+                                bool include_spaces = true, uint8_t interchar_space = 4);
+} // namespace utils
